e06aa760cf75b5aaf2831176e978eff59a326b60
MindstormingAB <s.elander@mindstorming.se>
2020-10-04 15:16:30 +0200

    Tests passed

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,14 @@
 export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
+  if (Array.isArray(stringArrayOrObject)) {
+    return stringArrayOrObject.length === 0;
+  }
   if (type === 'string') {
     return stringArrayOrObject === ''
   }
-
+  if (type === "object") {
+    return JSON.stringify(stringArrayOrObject) === JSON.stringify({});
+  }
   return false
 }
